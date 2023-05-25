15ce0b0d9c462933c96cd281289fbc4a3a9c9ac8
Keshav Sharma <83857069+keshav-07@users.noreply.github.com>
2023-05-25 22:05:35 +0530

    Update pattern3.java

--- Pattern/pattern3.java
+++ Pattern/pattern3.java
@@ -1,11 +1,27 @@
-import java.util.*;
+/*
+  Print a pattern like  N=5
+ 
+1 2 3 4 5 
+2 3 4 5
+3 4 5
+4 5
+5
+ */
 
-public class pattern3 {
-    
-    public static void main(String []args)throws Exception{
+import java.util.Scanner;
 
-        Scanner read =new Scanner(System.in);
-        int n=read.nextInt();
-        
+public class pattern4{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i=1;i<=5;i++)
+        {
+            for (int j=i;j<=n;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+        sc.close();
     }
 }
