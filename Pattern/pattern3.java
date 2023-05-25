fe25cf9ef8b15a14f3f153a80f91958bbed83f59
Keshav Sharma <83857069+keshav-07@users.noreply.github.com>
2023-05-25 22:02:52 +0530

    Add files via upload

---
+++ Pattern/pattern3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class pattern3 {
+    
+    public static void main(String []args)throws Exception{
+
+        Scanner read =new Scanner(System.in);
+        int n=read.nextInt();
+        
+    }
+}
